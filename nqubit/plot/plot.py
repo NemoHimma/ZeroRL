3516509270fe5b7836ece2d398da844d567fd270
NemoZhang <526348174@qq.com>
2020-12-15 15:32:25 +0800

    1.1 with smooth

--- nqubit/plot/plot.py
+++ nqubit/plot/plot.py
@@ -1,27 +1,41 @@
 # nbit-6, with truncation, one figure, all T.
+# with smooth
+
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
+smooth = True
+smooth_index = 100 # [0, max)
+threshold_len = 0 # == 10000??????????
+linewidth = 1.0
+
 def plot_func(dir, color, label):
-    event = EventAccumulator(dir)
-    event.Reload()
-    threshold = event.scalars.Items('threshold')
-    plt.plot([i.step for i in threshold], [i.value for i in threshold], color=color, label=label, linewidth=0.5)
-    return
+    if (smooth):
+        event = EventAccumulator(dir)
+        event.Reload()
+        threshold = event.scalars.Items('threshold')
+        threshold_len = len(threshold)
+        tmp_threshold = np.zeros(threshold_len)
+        for i in range(len(threshold)): # average with the behind
+            # threshold.value = threshold[i:i+smooth_index].value
+            # print(threshold[i])
+            tmp_threshold[i] = (np.array([j.value for j in threshold[i:i+smooth_index]])).mean()
+        # !!! threshold and tmp_threshold
+        plt.plot([i.step for i in threshold], tmp_threshold, color=color, label=label, linewidth=linewidth) 
+    else:
+        event = EventAccumulator(dir)
+        event.Reload()
+        threshold = event.scalars.Items('threshold')
+        plt.plot([i.step for i in threshold], [i.value for i in threshold], color=color, label=label, linewidth=linewidth)
+        return
 
 dir = "/home/pami/ZeroRL/nqubit/results/sac_energy_new/nbit-6/T-3.000/events.out.tfevents.1607676640.pami12"
 event = EventAccumulator(dir)
 event.Reload()
 print("\n\n")
-print("event_acc: ", event)
-print("ea.scalars.Keys(): ", event.scalars.Keys())
-
-threshold = event.scalars.Items('threshold')
-print("len(threshold): ", len(threshold))
-print("threshold[0:2]: ", threshold[0:2])
-print([(i.step, i.value) for i in threshold[0:11]])
-
+print("event: ", event)
+print("event.scalars.Keys(): ", event.scalars.Keys())
 
 # fig = plt.figure(figsize=(6,4))
 # ax1 = fig.add_subplot(211)
@@ -39,24 +53,17 @@ print([(i.step, i.value) for i in threshold[0:11]])
 # ax2 = fig.add_subplot(212)
 # ax2.plot([i.step for i in threshold], [i.value for i in threshold], color='b', label='T=3.5')
 
-plt.plot([i.step for i in threshold], [i.value for i in threshold], color='maroon', label='T=3.0')
-plt.xlim(0)
+plot_func(dir=dir, color='maroon', label='T=3.0')
+
 # acc=ea.scalars.Items('acc')
 # ax1.plot([i.step for i in acc],[i.value for i in acc],label='acc')
-plt.xlabel("step")
-plt.ylabel("threshold")
+
 
 dir2 = "/home/pami/ZeroRL/nqubit/results/sac_energy_new/nbit-6/T-3.500/events.out.tfevents.1607686863.pami12"
-event2 = EventAccumulator(dir2)
-event2.Reload()
-threshold2 = event2.scalars.Items('threshold')
-plt.plot([i.step for i in threshold2], [i.value for i in threshold2], color='steelblue', label='T=3.5')
+plot_func(dir=dir2, color='steelblue', label='T=3.5')
 
 dir3 = "/home/pami/ZeroRL/nqubit/results/sac_energy_new/nbit-6/T-4.000/events.out.tfevents.1607686837.pami12"
-event3 = EventAccumulator(dir3)
-event3.Reload()
-threshold3 = event3.scalars.Items('threshold')
-plt.plot([i.step for i in threshold3], [i.value for i in threshold3], color='g', label='T=4.0')
+plot_func(dir=dir3, color='g', label='T=4.0')
 
 dir4 = "/home/pami/ZeroRL/nqubit/results/sac_energy_new/nbit-6/T-4.500/events.out.tfevents.1607686906.pami12"
 plot_func(dir=dir4, color='gold', label='T=4.5')
@@ -70,14 +77,15 @@ plot_func(dir=dir6, color='sandybrown', label='T=7.0')
 dir7 = "/home/pami/ZeroRL/nqubit/results/sac_energy_new/nbit-6/T-9.000/events.out.tfevents.1607676748.pami12"
 plot_func(dir=dir7, color='pink', label='T=9.0')
 
+plt.xlim(0)
+plt.xlabel("step")
+plt.ylabel("threshold")
+
 plt.legend(loc='lower right')
 plt.savefig('original.jpg')
 
 plt.xlim(0, 15000)
 plt.savefig('truncation_x.jpg')
-
-plt.ylim(-2)
-plt.savefig('truncation_x_y.jpg')
 
 plt.xlim(0, 3000)
 plt.savefig('truncation_3000_-2.jpg')
